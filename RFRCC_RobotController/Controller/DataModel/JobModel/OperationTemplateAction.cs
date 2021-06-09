967e67e806781cc869f1bf16289df1ae60656c8a
Samuel McNeilly <samuel.mcneilly@gmail.com>
2021-06-09 11:41:47 +1000

    Generated Default Template Job

--- RFRCC_RobotController/Controller/DataModel/JobModel/OperationTemplateAction.cs
+++ RFRCC_RobotController/Controller/DataModel/JobModel/OperationTemplateAction.cs
@@ -10,7 +10,7 @@ namespace RFRCC_RobotController.Controller.DataModel
         public Type ExpectedOperationType { get; set; } //maybe enum? instead
         public int ExpIndex { get; set; }
         public int ExpIndexModifier { get; set; } = 0;
-        public bool recurring { get; set; } = false;
+        public bool Recurring { get; set; } = false;
     }
 
     public class JobModelTemplate
@@ -24,13 +24,33 @@ namespace RFRCC_RobotController.Controller.DataModel
     /// </summary>
     public class DefaultJobTemplate : JobModelTemplate
     {
+        /// <summary>
+        /// Constructor to generate Job Template with no name
+        /// </summary>
         public DefaultJobTemplate() : this("")
         {
         }
 
+        /// <summary>
+        /// Constructor to generate Job Template
+        /// </summary>
+        /// <param name="name">Name of process for ID</param>
         public DefaultJobTemplate(string name)
         {
             Dictionary<string, string> NextAttributes = new Dictionary<string, string>();
+
+            /* 
+             * 1. start job - capture time and date
+             * 2. PLC - Load Material
+             * 3. PLC - Drive stock into cell
+             * 4. PLC - measure stock lenght
+             * 5. PLC - drive stock for probe
+             * 6. PLC - Close Vice & zero
+             * 7. Robot - probe stock for sizes
+             * (R) 8. PLC - drive stock to required delta-x
+             * (R) 8+1. Robot - Make Cut
+             * 9. PLC - Drive stock out of cell
+             */
 
             Name = name;
             TemplateListofOperations.Add(new OperationTemplateAction()
@@ -44,6 +64,7 @@ namespace RFRCC_RobotController.Controller.DataModel
             NextAttributes.Clear();
             NextAttributes.Add("MaterialName", "NULL");
             NextAttributes.Add("MinimumStockLength", "NULL");
+            NextAttributes.Add("Condition", "Wait for Operator OK");
             TemplateListofOperations.Add(new OperationTemplateAction()
             {
                 Name = "Load Material onto Infeed",
@@ -77,7 +98,7 @@ namespace RFRCC_RobotController.Controller.DataModel
             NextAttributes.Add("Infeed", "Drive until Stock X @ ROBOTUPDATE:STOCK_X");
             TemplateListofOperations.Add(new OperationTemplateAction()
             {
-                Name = "Measure stock length",
+                Name = "Drive stock into cell for checking",
                 ExpectedOperationType = typeof(OperationPLCProcess),
                 ExpIndex = 5,
                 Attributes = NextAttributes // this might need to be cloned
@@ -85,16 +106,44 @@ namespace RFRCC_RobotController.Controller.DataModel
 
             NextAttributes.Clear();
             NextAttributes.Add("InfeedVice", "Clamp Stock");
-            NextAttributes.Add("InfeedVice", "Clamp Stock");
+            NextAttributes.Add("InfeedViceEncoder", "Zero");
             TemplateListofOperations.Add(new OperationTemplateAction()
             {
-                Name = "Measure stock length",
+                Name = "Close Infeed Clamp",
                 ExpectedOperationType = typeof(OperationPLCProcess),
-                ExpIndex = 5,
+                ExpIndex = 6,
                 Attributes = NextAttributes // this might need to be cloned
             });
 
+            NextAttributes.Clear();
+            TemplateListofOperations.Add(new OperationTemplateAction()
+            {
+                Name = "Measure stock dimensions",
+                ExpectedOperationType = typeof(OperationRobotProcess),
+                ExpIndex = 7
+            });
 
+            NextAttributes.Clear();
+            NextAttributes.Add("Condition", "IF stock DX < NextDX");
+            NextAttributes.Add("Infeed", "Drive until Stock DX @ NextDX");
+            TemplateListofOperations.Add(new OperationTemplateAction()
+            {
+                Name = "Drive Stock to next position",
+                ExpectedOperationType = typeof(OperationPLCProcess),
+                Recurring = true,
+                ExpIndex = 8,
+                Attributes = NextAttributes // this might need to be cloned
+            });
+
+            NextAttributes.Clear();
+            TemplateListofOperations.Add(new OperationTemplateAction()
+            {
+                Name = "Cut Feature",
+                ExpectedOperationType = typeof(OperationRobotManoeuvre),
+                Recurring = true,
+                ExpIndex = 8,
+                ExpIndexModifier = 1
+            });
         }
     }
 }
