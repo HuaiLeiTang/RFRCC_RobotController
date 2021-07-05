2729557555697789445139ab5c1e1d4b073439cb
Samuel McNeilly <samuel.mcneilly@gmail.com>
2021-07-05 10:58:48 +1000

    operationAction included with interactions for start stop and skip, and associated event handlers

--- RFRCC_RobotController/Controller/DataModel/JobModel/OperationAction.cs
+++ RFRCC_RobotController/Controller/DataModel/JobModel/OperationAction.cs
@@ -11,18 +11,114 @@ namespace RFRCC_RobotController.Controller.DataModel
     /// </summary>
     public class OperationAction : ICloneable
     {
+        private bool _complete = false;
+        private bool _skip = false;
+        private bool _processing = false;
+        
+        
         /// <summary>
         /// Key text of the operation action
         /// </summary>
         public string Name { get; set; }
         /// <summary>
+        /// Status if Action has been completed
+        /// </summary>
+        public bool Completed 
+        { 
+            get => _complete;
+            set 
+            {
+                _complete = value;
+                if (_complete)
+                {
+                    _processing = false;
+                    OnActionCompleted();
+                }
+            } 
+        }
+        /// <summary>
+        /// Status of Action is to be skipped
+        /// </summary>
+        public bool Skip 
+        {
+            get => _skip;
+            set 
+            {
+                _skip = value;
+                OnActionSkipUpdated();
+            } 
+        }
+        /// <summary>
+        /// Status of Actions execution
+        /// </summary>
+        public bool Processing 
+        { 
+            get => _processing; 
+        }
+        /// <summary>
         /// Attributes of the Operation sorted by key and string value
         /// </summary>
         public Dictionary<string,string> Attributes { get; set; }
-
+        /// <summary>
+        /// Event when Action is completed
+        /// </summary>
+        public EventHandler ActionCompleted;
+        /// <summary>
+        /// Event when Action is started
+        /// </summary>
+        public EventHandler ActionStarted;
+        /// <summary>
+        /// Event when skip status of this action is changed
+        /// </summary>
+        public EventHandler ActionSkipUpdated;
+        /// <summary>
+        /// Begins Actions Processing and raises start event
+        /// </summary>
+        public void Start()
+        {
+            _processing = true;
+            OnActionStart();
+        }
+        /// <summary>
+        /// Stops processing of Action marks complete if action was successful
+        /// </summary>
+        public void Complete(bool success = true)
+        {
+            _processing = false;
+            Completed = success;
+        }
         public object Clone()
         {
             return this.MemberwiseClone();
         }
+
+
+
+        // Internal Event Handling
+        /// <summary>
+        /// raise event ActionStarted if Action.Processing == true
+        /// </summary>
+        protected virtual void OnActionStart()
+        {
+            if (_processing)
+            {
+                ActionStarted?.Invoke(this, new EventArgs());
+            }
+        }
+        /// <summary>
+        /// Raise event ActionFinished
+        /// </summary>
+        protected virtual void OnActionCompleted()
+        {
+            ActionCompleted?.Invoke(this, new EventArgs());
+        }
+        /// <summary>
+        /// Raise event ActionSkipUpdated
+        /// </summary>
+        protected virtual void OnActionSkipUpdated()
+        {
+            ActionSkipUpdated?.Invoke(this, new EventArgs());
+        }
+
     }
 }
